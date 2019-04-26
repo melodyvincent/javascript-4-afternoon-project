43ad3b7aa1377936e9a1d47806d84bde830deeb4
Melody Vincent <melodyvincent@Melodys-MacBook-Pro.local>
2019-04-26 15:36:09 -0600

    classes.js commit

--- classes.js
+++ classes.js
@@ -30,7 +30,19 @@
 */
 
 //Code Here
+class Employee{
+  constructor(first_name, last_name, email, age){
+      this.first_name = first_name;
+      this.last_name = last_name;
+      this.email = email;
+      this.age = age;
+      this.makeWidget = function(){
+          return first_name + ' ' + last_name + ' Widget';
+  }
+  }
+}var person = new Employee('Melody', 'Vincent','mmfcecil003@gmail.com',100);
 
+console.log(person);
 
 ////////// PROBLEM 2 //////////
 
@@ -48,6 +60,25 @@
 */
 
 //Code Here
+
+
+class Manager extends Employee{
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age)
+    this.reports = [];
+    
+  }
+  hire(employee) {
+    this.reports.push(employee);
+  }
+  fire(index) {
+    this.reports.splice(index, 1);
+
+  }
+
+}
+let me = new Manager("Melody", "Vincent", "gmail.com", 15)
+console.log(me)
 
 
 ////////// PROBLEM 3 //////////
@@ -72,7 +103,32 @@
 */
 
 //Code Here
-
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age, reports)
+    this.title = "Not a manager";
+    this.bonus = 0;
+  }
+  hire(){
+    super.hire();
+      if (this.reports.length <= 3){
+        this.title = "Barely Manager";
+  
+      }else if(this.reports.length <=10) {
+        this.title = 'Mostly Manager';
+      } else if(this.reports.length <=50){
+        this.title = "Manager";
+      }else if(this.reports.length <= 100){
+        this.title = "Manager Plus";
+      }else if(this.reports.length >=101){
+        this.title = "Bestest Manager";
+      }
+  }
+  fire(){
+    super.fire();
+     this.bonus += 100;
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -100,4 +156,15 @@
 
 //Code Here
 
+// class Machine {
+//   constructor (widgets_made_count, wear_and_tear_count, needs_reboot) {
+//     this.widgets_made_count = widgets_made_count;
+//     this.wear_and_tear_count = wear_and_tear_count;
+//     this.needs_reboot = needs_reboot;
+//     this.makeWidget = function(){
+// //       for (let i = 0; i )
+      
+// //     }
+// //   }
 
+// // }
